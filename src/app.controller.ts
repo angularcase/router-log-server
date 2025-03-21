dbaa056e1c7aab42a8e0e2217b65879084852e71
tomasz.dobosz <tomasz.dobosz@tiskel.com>
2025-03-21 18:58:37 +0100

    na nowo

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -1,49 +1,12 @@
 import { Controller, Get } from '@nestjs/common';
-import * as fs from 'fs';
-import * as path from 'path';
-import { LineData, MacAddress } from './syslog-service.service';
+import { AppService } from './app.service';
 
 @Controller()
 export class AppController {
-  private readonly LOG_FILE = path.join(__dirname, '../assets/', 'logs.json');
+  constructor(private readonly appService: AppService) {}
 
   @Get('live-data')
-  liveData(): any {
-    if (fs.existsSync(this.LOG_FILE)) {
-      try {
-        const content = fs.readFileSync(this.LOG_FILE, 'utf8');
-        const logs = JSON.parse(content) as LineData[];
-
-        const result: {
-          [key: string]: { state: 'in' | 'out'; date: string };
-        } = {};
-
-        for (const key of Object.keys(MacAddress)) {
-          let state: 'in' | 'out' = 'out';
-          let date = '';
-          const mac = MacAddress[key as keyof typeof MacAddress];
-
-          for (let i = logs.length - 1; i >= 0; i--) {
-            const entry = logs[i];
-            if (entry.mac_addr.toLowerCase() === mac.toLowerCase()) {
-              if (entry.action === 'Auth') {
-                state = 'in';
-              } else if (entry.action === 'Deauth_ind') {
-                state = 'out';
-              }
-              date = entry.date;
-              break;
-            }
-          }
-          result[key] = { state: state, date: date };
-        }
-
-        return result;
-      } catch (error) {
-        return { error: 'Błąd podczas parsowania pliku logs.json' };
-      }
-    } else {
-      return { message: 'Plik logs.json nie istnieje.' };
-    }
+  getLiveData() {
+    return this.appService.getLiveData();
   }
 }
